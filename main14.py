14841d4d7f9236fe6d83dc287e2a3eaf2dffa205
radinracer55 <b_shoaibi@yahoo.com>
2024-09-17 15:21:40 +0330

    Add files via upload
    AddProject

---
+++ main14.py
@@ -0,0 +1,9 @@
+name = input("enter your name: ")
+message = input("login/departure/morekhasi?:")
+from datetime import datetime
+now = datetime.now().time() 
+print("now =", now)
+print("type(now) =", type(now))
+if message == "morekhasi": 
+    input("as key ta key: ")
+   
